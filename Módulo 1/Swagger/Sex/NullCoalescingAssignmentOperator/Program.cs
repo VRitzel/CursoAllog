9997597c32cbdaed4dd1556c2d1b25c3795ec251
VRitzel <114676089+VRitzel@users.noreply.github.com>
2023-05-26 17:58:12 -0300

    Add files via upload

---
+++ Módulo 1/Swagger/Sex/NullCoalescingAssignmentOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿string name1 = null;
+string name2 = null;
+string name3 = "Richard";
+string defaultname = "Mark";
+
+if(name1 == null){
+    name1 = defaultname;
+}
+Console.WriteLine(name1);
+name2 ??= defaultname;
+Console.WriteLine(name2);
+
+name3 ??= defaultname;
+Console.WriteLine(name3);
